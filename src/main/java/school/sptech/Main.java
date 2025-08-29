1d503389f28b560a138ec2ab3bb25d09409b4d17
VitorioBearari <vitorio.bearari@sptech.school>
2025-08-29 14:01:38 -0300

    Respondendo a atividade Gerenciamento de desempenho

--- src/main/java/school/sptech/Main.java
+++ src/main/java/school/sptech/Main.java
@@ -1,8 +1,20 @@
 package school.sptech;
 
 public class Main {
-
     public static void main(String[] args) {
+        Time corinthians = new Time();
+
+        corinthians.registrarVitoria();
+        corinthians.registrarVitoria();
+        corinthians.registrarEmpate();
+        corinthians.registrarEmpate();
+        corinthians.registrarEmpate();
+        corinthians.registrarEmpate();
+        corinthians.registrarDerrota();
+
+        System.out.println(corinthians.getPontos());
+        System.out.println(corinthians.getTotalPartidas());
+        System.out.println(corinthians.getAproveitamento());
 
     }
 }
--- src/main/java/school/sptech/Time.java
+++ src/main/java/school/sptech/Time.java
@@ -1,5 +1,50 @@
 package school.sptech;
 
 public class Time {
+    String nome;
+    Integer vitorias, empates, derrotas;
 
+    public Time(){
+        vitorias = 0;
+        empates = 0;
+        derrotas = 0;
+    }
+
+    public void registrarVitoria(){
+        vitorias += 1;
+    }
+
+    public void registrarEmpate(){
+        empates += 1;
+    }
+
+    public void registrarDerrota(){
+        derrotas += 1;
+    }
+
+    public Integer getPontos(){
+        return (vitorias*3) + empates;
+    }
+
+    public Integer getTotalPartidas(){
+        int totalPartidas = vitorias + empates + derrotas;
+        return totalPartidas;
+    }
+
+    public Integer getAproveitamento(){
+        Integer aproveitamento = (vitorias*100 / getTotalPartidas());
+        return aproveitamento;
+    }
+
+    public void compararAproveitamento(Time adversario){
+        if(getAproveitamento() > adversario.getAproveitamento()){
+            System.out.println("Time " +nome+ " tem um aproveitamento maior que o time "+adversario.nome+".");
+        } else {
+            System.out.println("Time " +adversario.nome+ " tem um aproveitamento maior que o time "+nome+".");
+        }
+    }
+
+    public void exibirInformacoes(){
+        System.out.println("Nome: "+nome+"\nNúmero de vitórias: "+vitorias+"\nNúmero de Empates: "+empates+"\nNúmero de Derrotas: "+derrotas+"\nTotal de partidas: "+(vitorias+empates+derrotas)+"\nAproveitamento: "+getAproveitamento());
+    }
 }
